8d6f239a7b24cee2c6ac56ac60b4faa4cba8b661
Snehal Saurabh <113467836+snehalsaurabh@users.noreply.github.com>
2024-05-07 16:39:18 +0530

    Created the setup for Mongoose Atlas

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,24 @@
 // Initialization
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
 const app = express();
 
-//Home Route (/)
-app.get('/', function(req, res) {
-    res.send("This is the Home page");
-});
+mongoose.connect(process.env.URL).then(function(){
+    // Home Route (/)
+    app.get('/', function(req, res) {
+        res.send("This is the Home page");
+    });
 
-// Notes Route
-app.get('/notes', function(req, res) {
-    res.send("This is the Notes page");
-});
+    // Notes Route
+    app.get('/notes', function(req, res) {
+        res.send("This is the Notes page");
+    });
 
-// Starting the server on a PORT
-app.listen(5000, function(){
-    console.log("Server started at Port: 5000");
+    // Starting the server on a PORT
+    app.listen(5000, function(){
+        console.log("Server started at Port: 5000");
+    });
 });
