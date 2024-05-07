5c3add26a503faec1686e9795b42c82821588741
Snehal Saurabh <113467836+snehalsaurabh@users.noreply.github.com>
2024-05-07 15:48:36 +0530

    Basic express server setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,18 @@
+// Initialization
 const express = require('express');
 const app = express();
 
-app.listen(5000);
+//Home Route (/)
+app.get('/', function(req, res) {
+    res.send("This is the Home page");
+});
+
+// Notes Route
+app.get('/notes', function(req, res) {
+    res.send("This is the Notes page");
+});
+
+// Starting the server on a PORT
+app.listen(5000, function(){
+    console.log("Server started at Port: 5000");
+});
